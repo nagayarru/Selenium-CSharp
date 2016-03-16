d278caba07c09c40ffd103c5fae4180ba86c9b24
nagayarru <naga4qa@gmail.com>
2016-03-16 13:50:32 -0500

    added solution

---
+++ SeleniumConsoleApplication/Selenium-CSharp/README.md
@@ -0,0 +1,7 @@
+# Selenium-CSharp
+Selenium using C sharp 
+Will be writing the Selenium tests with C Sharp in this repo.
+Just making sure I can edit with cmd and push the changes.
+Checking that I can edit this from GitHub and pull the changes in local.
+
+Got the local copy again from GitHub in Windows machine and pushing it back to GitHub.
---
+++ SeleniumConsoleApplication/SeleniumConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+
+
+namespace SeleniumConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver x = new ChromeDriver();
+            x.Navigate().GoToUrl("http://www.bing.com");
+            x.Quit();
+        }
+    }
+}
